6755d1f5b11a326f45a539ab72c2d09369edd5eb
Ben Katz <benkatz94@gmail.com>
2016-12-12 01:40:29 -0500

    Made Fast LUT Parfor-able

--- Fast_LUT_Test.m
+++ Fast_LUT_Test.m
@@ -22,7 +22,7 @@ pmi = linspace(1, 0, n2);   %Percent Max current.  Easier to generate table this
 mag_vec = linspace(1, sqrt(3/2)*200, n2);
 %phase = linspace(phase_min, phase_max, n);
 
-
+mag_vec = zeros(n3, 1);
 mag_log_vec = zeros(n2, n3);
 phi_max_vec = zeros(n2, n3);
 mag_max_vec = zeros(n2, n3);
@@ -31,18 +31,67 @@ torque_max_vec = zeros(n2, n3);
 tdot_vec = zeros(n2, n3);
 i_mag_vec =  zeros(n2, n3);
 percent = zeros(n2, n3);
-%%% Find max achievable current and pos/neg torques vs speed %%%
+
+%%% Find max achievable current and pos/neg torques vs speed %%%    parfor k = 1:n3
 for k = 1:n3
-    
     tic
-    for j = 1:n2
+    phi = linspace(phase_min, phase_max, 5)';
+    delta = phi(2) - phi(1);
+    torque = zeros(5, 1);
+    mag = zeros(5, 1);
+    parfor x = 1:5
+        [torque(x), mag(x)] = motor_fun(mag_max, phi(x), tdot(k));
+    end
+    tmax = max(torque);
+    ind_max = find(torque==tmax);
+    ind_max = ind_max(1);
+    ind = [ind_max-1; ind_max; ind_max+1];
+    while(delta > tol)
+        delta = delta/2;
+
+        if(ind(3) > length(phi))
+            phi = circshift(phi, -1);
+            torque = circshift(torque, -1);
+            mag = circshift(mag, -1);
+            ind = ind-1;
+        elseif (ind(1)==0)
+            phi = circshift(phi, 1);
+            torque = circshift(torque, 1);
+            mag = circshift(mag, 1);
+            ind = ind+1;
+        end
+
+        phi = [phi(ind(1)); phi(ind(1))+delta; phi(ind(2)); phi(ind(2))+delta; phi(ind(3))];
+        [t1, m1] = motor_fun(mag_max, phi(2), tdot(k));
+        [t2, m2] = motor_fun(mag_max, phi(4), tdot(k));
+        torque = [torque(ind(1));t1; torque(ind(2)); t2; torque(ind(3))];
+        mag = [mag(ind(1)); m1; mag(ind(2)); m2; mag(ind(3))];
+        tmax = max(torque);
+        ind_max = find(torque==tmax);
+        ind_max = ind_max(1);
+        ind = [ind_max-1; ind_max; ind_max+1];
+        %torque_vec = [torque_vec;torque];
+        %phase_vec = [phase_vec;phase];
+        torque_max_vec(1, k) = max(torque);
+        phi_max_vec(1, k) = phi(ind_max);
+        mag_max_vec(1, k) = mag(ind_max);
+        tdot_vec(1, k) = tdot(k);
+    end   
+end
+
+max_current = mag_max_vec(1,:);
+
+tic
+parfor j = 1:n2
+    
+    for k = 1:n3
         phi = linspace(phase_min, phase_max, 5)';
         delta = phi(2) - phi(1);
         torque = zeros(5, 1);
         mag = zeros(5, 1);
         
-        parfor x = 1:5
-            [torque(x), mag(x)] = motor_fun(mag_max*(j==1) + mag_max_vec(j, 1)*(j>1)*pmi(j), phi(x), tdot(k));
+        for x = 1:5
+            [torque(x), mag(x)] = motor_fun(max_current(k)*pmi(j), phi(x), tdot(k));
         end
         tmax = max(torque);
         ind_max = find(torque==tmax);
@@ -64,8 +113,8 @@ for k = 1:n3
             end
 
             phi = [phi(ind(1)); phi(ind(1))+delta; phi(ind(2)); phi(ind(2))+delta; phi(ind(3))];
-            [t1, m1] = motor_fun(mag_max*(j==1) + mag_max_vec(j, 1)*(j>1)*pmi(j), phi(2), tdot(k));
-            [t2, m2] = motor_fun(mag_max*(j==1) + mag_max_vec(j, 1)*(j>1)*pmi(j), phi(4), tdot(k));
+            [t1, m1] = motor_fun(max_current(k)*pmi(j), phi(2), tdot(k));
+            [t2, m2] = motor_fun(max_current(k)*pmi(j), phi(4), tdot(k));
             torque = [torque(ind(1));t1; torque(ind(2)); t2; torque(ind(3))];
             mag = [mag(ind(1)); m1; mag(ind(2)); m2; mag(ind(3))];
             tmax = max(torque);
@@ -76,16 +125,18 @@ for k = 1:n3
             %phase_vec = [phase_vec;phase];
             torque_max_vec(j, k) = max(torque);
             phi_max_vec(j, k) = phi(ind_max);
-            mag_max_vec(j, k) = mag_max*(j==1) + mag_max_vec(j, 1)*(j>1)*pmi(j);%mag(ind_max);
+            mag_max_vec(j, k) = max_current(k)*pmi(j);%mag(ind_max);
             tdot_vec(j, k) = tdot(k);
         end   
     end
 
-    toc
+    
 end
+toc
 
-%figure;plot(tdot, torque_max_vec); title('Max Torque')
-%figure;plot(tdot, phi_max_vec);  title('Max Phase')
+%figure;plot(tdot, torque_max_vec(1,:)); title('Max Torque')
+%figure;plot(tdot, phi_max_vec(1,:));  title('Max Phase')
+%figure;plot(tdot, mag_max_vec(1,:)); title('Max Current')
 figure; surf(tdot_vec, mag_max_vec, torque_max_vec);
 figure;surf(tdot_vec, mag_max_vec, phi_max_vec);
 % figure;plot(tdot, i_mag_max_vec); title('Max Current')%figure;plot(tdot, t_max_vec, tdot, t_min_vec); title('Max/Min Torque')
